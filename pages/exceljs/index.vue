913e5336a2357f82e3ffef98bc9949e1a0d801a5
Nattapat Niyomthai <nattapat@focalsolution.com>
2022-01-13 15:52:55 +0700

    feat: lib exceljs

---
+++ pages/exceljs/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div>Export Excell</div>
+        <button @click="onExport">download</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            exportData:[
+                { name: 'Dady', age: '21' ,test: "home" },
+                { name: 'Jonh', age: '25' },
+                { name: 'James', age: '17' },
+            ],
+        }
+    },
+    methods:{
+        onExport(){
+        }
+    }
+}
+</script>
--- pages/xlsx/index.vue
+++ pages/xlsx/index.vue
@@ -11,18 +11,30 @@ export default {
     data() {
         return {
             exportData:[
-                { name: 'Dady', age: '21' },
+                { name: 'Dady', age: '21' ,test: "home" },
                 { name: 'Jonh', age: '25' },
                 { name: 'James', age: '17' },
-            ]
+            ],
         }
     },
     methods:{
         onExport(){
-            const dataWS = XLSX.utils.json_to_sheet(this.exportData)
+            // let dataWS = XLSX.utils.aoa_to_sheet(this.ws_data)
+            // const dataWS = XLSX.utils.json_to_sheet(this.exportData)
+            const dataWS = {
+                A1: {
+                    "v":"Time Period : 31/02/2021 15:00 - 01/04/2021 15:00",
+                    "t":"s",
+                    "s": {
+                        font: {
+                            sz: "24",
+                        },
+                    }},
+                U1: {"v":"Correct answer","t":"s"},V1: {"v":"10","t":"n"},"!ref":"A1:V1" }
+            console.log(JSON.stringify(dataWS))
             const wb = XLSX.utils.book_new()
             XLSX.utils.book_append_sheet(wb, dataWS)
-            XLSX.writeFile(wb,'export.xlsx')
+            XLSX.writeFile(wb,'export.ods')
         }
     }
 }

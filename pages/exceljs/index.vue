b321f2dcb22f473ff79ee6fffdd7dd82b93e997b
Nattapat Niyomthai <nattapat@focalsolution.com>
2022-01-13 16:30:58 +0700

    feat: export xlsx via exceljs

--- pages/exceljs/index.vue
+++ pages/exceljs/index.vue
@@ -6,6 +6,9 @@
 </template>
 
 <script>
+import * as Excel from "exceljs/dist/exceljs.min.js";
+import * as ExcelProper from "exceljs";
+import * as FileSaver from 'file-saver';
 export default {
     data() {
         return {
@@ -17,7 +20,25 @@ export default {
         }
     },
     methods:{
-        onExport(){
+        async onExport(){
+            let workbook= new Excel.Workbook();
+            var worksheet = workbook.addWorksheet('My Sheet');
+
+            worksheet.columns = [
+            { header: 'Id', key: 'id', width: 10 },
+            { header: 'Name', key: 'name', width: 32 },
+            { header: 'D.O.B.', key: 'DOB', width: 10 }
+            ];
+            worksheet.getCell('A1').fill = {
+                type: 'pattern',
+                pattern:'darkVertical',
+                fgColor:{argb:'FFFF00'}
+            };
+            worksheet.addRow({ id: 1, name: 'Ionic Android', dob: new Date(1970, 1, 1) });
+            worksheet.addRow({ id: 2, name: 'Ionic iOS', dob: new Date(1965, 1, 7) });
+            var tempFilePath = 'PATH/temp.xlsx'; // PATH is where you want to create your file
+
+            workbook.xlsx.writeBuffer().then(buffer => FileSaver.saveAs(new Blob([buffer]), `${Date.now()}_feedback.xlsx`)).catch(err => console.log('Error writing excel export', err))
         }
     }
 }

4138ea06465e932e32e9fba1692d6868145380f9
Nattapat Niyomthai <nattapat@focalsolution.com>
2022-01-13 11:41:40 +0700

    feat: export basic xlsx

---
+++ pages/xlsx/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div>Export Excell</div>
+        <button @click="onExport">download</button>
+    </div>
+</template>
+
+<script>
+import XLSX from 'xlsx' // import xlsx
+export default {
+    data() {
+        return {
+            exportData:[
+                { name: 'Dady', age: '21' },
+                { name: 'Jonh', age: '25' },
+                { name: 'James', age: '17' },
+            ]
+        }
+    },
+    methods:{
+        onExport(){
+            const dataWS = XLSX.utils.json_to_sheet(this.exportData)
+            const wb = XLSX.utils.book_new()
+            XLSX.utils.book_append_sheet(wb, dataWS)
+            XLSX.writeFile(wb,'export.xlsx')
+        }
+    }
+}
+</script>
